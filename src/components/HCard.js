4a2ffeca8a2b50f261b4e146d34e901aa1b8e1b7
Andy134 <tuananh01499@gmail.com>
2023-01-21 20:53:50 +0700

    w

--- src/components/HCard.js
+++ src/components/HCard.js
@@ -11,16 +11,16 @@ export default function HCard({hisItm}) {
 
     return <div className="col-sm-12 col-md-9 py-2">
         {hisItm.type === "I" ?
-        <div class="card d-flex align-items-start">
-            <div class="card-body">
-                <h4 class="card-title"><strong className="text-success">+ {util.getLocalCurrency(+hisItm.amount)}</strong> </h4>
+        <div className="card d-flex align-items-start">
+            <div className="card-body">
+                <h4 className="card-title"><strong className="text-success">+ {util.getLocalCurrency(+hisItm.amount)}</strong> </h4>
                 {cardText}
             </div>
         </div>
         :
-        <div class="card d-flex align-items-end">
-            <div class="card-body d-flex flex-column align-items-end">
-                <h4 class="card-title"><strong className="text-danger">- {util.getLocalCurrency(+hisItm.amount)}</strong></h4>
+        <div className="card d-flex align-items-end">
+            <div className="card-body d-flex flex-column align-items-end">
+                <h4 className="card-title"><strong className="text-danger">- {util.getLocalCurrency(+hisItm.amount)}</strong></h4>
                 {cardText}
             </div>
         </div>
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,7 +1,10 @@
+import moment from "moment";
 import { createContext, useEffect, useState } from "react";
+import { CSVLink } from "react-csv";
 import HCard from "../components/HCard";
 import { packService } from '../service/pack.service';
-export const AppContext = createContext() 
+
+export const AppContext = createContext() ;
 
 export default function History() {
   
@@ -15,8 +18,19 @@ export default function History() {
 
   return (
       <div className="history">
+        <div className="row d-flex justify-content-end">
+          <div className="col-3 d-flex justify-content-end">
+            <CSVLink 
+              className="btn btn-success"
+              data={history}
+              filename={`History_${moment().format("DDMMYYYYHHMMSS").toString()}.csv`}
+            >
+              Download
+            </CSVLink>
+          </div>
+        </div>
         <div className="row d-flex justify-content-center py-2">
-            {history.map((item)=><HCard hisItm={item}/>)}
+          {history.map((item)=><HCard hisItm={item}/>)}
         </div>
       </div>
   );
