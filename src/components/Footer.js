3211dbe81df10083f1ca6ec4ef8ea93476d5364d
Andy <tuananh01499@gmail.com>
2023-03-15 14:03:14 +0700

    1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import "./../assets/footer.css"
 export default function Footer() {
 
     return <>
-      <div className="footer container my-3 d-flex justify-content-start align-items-center">
-          Snowy App
+      <div className="footer container my-3 d-flex justify-content-start align-items-center">&nbsp;
       </div>
     </>
 }
